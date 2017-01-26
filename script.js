d5f6484fa8bfa11d16273606c96023115958a7aa
juliakent77 <juliakent@Julias-MacBook-Air.local>
2017-01-25 18:24:54 -0800

    Trying some stuff out

---
+++ script.js
@@ -0,0 +1,45 @@
+// How to get the div clicks to input tags within my array?
+// How to add event listeners on my divs
+
+
+// An array of my recipes:
+// [{name:'Old Fashioned', image:'src="images/drank.jpg"', spirit: 'Whiskey', flavor: 'sweet', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}]
+
+
+
+
+
+
+// // Random Int function
+// var getRandomInt = function(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// var generateComputerChoice = function() {
+//   var randNum = getRandomInt(1, 3);
+//   if(randNum === 1) {
+//     computerChoice = 'rock';
+//   } else if (randNum === 2) {
+//     computerChoice = 'paper';
+//   } else {
+//     computerChoice = 'scissors';
+//   }
+
+//   checkWinner();
+// }
+
+// var whiskeyButton = document.getElementById('whiskey_button');
+
+// whiskeyButton.addEventListener('click', function() {
+//   console.log ('Hi');
+// })
+
+// var whiskeyButton = $( "#whiskey_button" )
+
+// $('whiskeyButton').click(function(event) {
+//   console.log('Clicked!');
+// });
+
+$("whiskey_button").click(function() {
+    console.log('clicked');
+});
